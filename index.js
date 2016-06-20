369be019796e7d0fb66285d8c0116a006db91ad4
spat-ne-hochu <spat.ne.hochu@gmail.com>
2016-06-20 19:15:24 +0300

    update directive provider to class, add method define for special case

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-function provide(name) {
-    let cwd = process.cwd();
+/**
+ * @module
+ */
 
-    try {
-        return require(`${cwd}/combine/directives/${name}`);
-    } catch (e) {
-        try {
-            return require(`${cwd}/combine/directives/${name}-directive`);
-        } catch (e) {
-            return require(`@combinejs/${name}-directive`);
+class DirectiveProvider {
+    constructor() {
+        this._directives = {};
+    }
+
+    /**
+     * Get directive by name
+     * you can has project scope directive
+     * for this place him to next paths:
+     *   1. [PROJECT_DIR]/combine/directives/[NAME].js
+     *   2. [PROJECT_DIR]/combine/directives/[NAME]/index.js
+     *   3. [PROJECT_DIR]/combine/directives/[NAME]-directive.js
+     *   4. [PROJECT_DIR]/combine/directives/[NAME]-directive/index.js
+     *
+     * @param name {String} directive name
+     * @returns {*}
+     */
+    prodive(name) {
+        let cwd = process.cwd();
+
+        if (! this._directives[name]) {
+            try {
+                this._directives[name] = require(`${cwd}/combine/directives/${name}`);
+            } catch (e) {
+                try {
+                    this._directives[name] = require(`${cwd}/combine/directives/${name}-directive`);
+                } catch (e) {
+                    this._directives[name] = require(`@combinejs/${name}-directive`);
+                }
+            }
         }
+
+        return this._directives[name];
+    }
+
+    /**
+     * For special case you can define directive manual
+     *
+     * @param name {String} directive name
+     * @param directive {LikeDirectiveObject}
+     */
+    define(name, directive) {
+        this._directives[name] = directive;
     }
 }
 
-module.exports = provide;
+module.exports = DirectiveProvider;
 
